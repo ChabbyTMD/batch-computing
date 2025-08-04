46c65889150f5205f9f7dac84d204f9b6fa76fa1
ChabbyTMD <greena.mugoya@gmail.com>
2025-08-04 14:03:01 -0700

    Changed to appropriate reservation

---
+++ roll-4-pi_demo.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=roll-4-pi
+#SBATCH --account=gue998
+#SBATCH --reservation=si25cpu
+#SBATCH --partition=shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --time=00:02:00
+#SBATCH --output=%x.o%j.%N
+
+module reset
+module load gcc/10.2.0
+module load python/3.8.12
+module list
+printenv
+
+time -p python3 pi.py 100000000
